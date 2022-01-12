6db7c0a41a563e326ea73b1f14811a6ca7abd00c
Deepak76320 <94068572+Deepak-iiitg@users.noreply.github.com>
2022-01-12 14:30:27 +0530

    Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+var n = parseInt(prompt("enter a number"));
+
+for ( var i=1 ; i<=n ; i++){
+
+    for(var j=1;j<i+1;j++){
+       document.write("* ");
+    }
+    document.write("<br>");
+    
+}
